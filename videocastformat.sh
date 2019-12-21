1bcd3446f8cf9bc9239d0d64b811ad6aad5d5d21
Felipe Facundes <felipe.facundes@gmail.com>
2019-12-21 20:42:38 -0300

    Update videocastformat.sh

--- videocastformat.sh
+++ videocastformat.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 mkdir chcastvideos;
 
-for i in *.[mM][kK][vV][pP][4];
+for i in *.[mM][kK][vV];
   do name=`echo $i | cut -d'.' -f1`;
   echo $name;
   ffmpeg -i "$i" -vcodec copy -acodec mp3 -n "${name}.chcast.mp4";
-done
+done;
+
+for i in *.[mM][pP][4];
+  do name=`echo $i | cut -d'.' -f1`;
+  echo $name;
+  ffmpeg -i "$i" -vcodec copy -acodec mp3 -n "${name}.chcast.mp4";
+done;
 
 sleep 1;
 
